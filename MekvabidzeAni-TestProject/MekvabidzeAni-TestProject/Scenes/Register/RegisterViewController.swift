5deddb266dabb77b36b683ad63cb82fb1dfc30e9
AniMekvab <vottakhoney@gmail.com>
2023-11-12 01:07:19 +0400

    clean up

--- MekvabidzeAni-TestProject/MekvabidzeAni-TestProject/Scenes/Register/RegisterViewController.swift
+++ MekvabidzeAni-TestProject/MekvabidzeAni-TestProject/Scenes/Register/RegisterViewController.swift
@@ -2,122 +2,98 @@
 //  RegisterViewController.swift
 //  MekvabidzeAni-TestProject
 //
-//  Created by Ani Mekvabidze on 11/10/23.
+//  Created by Ani Mekvabidze on 11/11/23.
 //
 
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    // MARK: - Variables
+    
+    private var viewModel: RegisterViewModel
+    private var email: String { emailTextField.textField.text ?? "" }
+    private var age: String { ageTextField.textField.text ?? "" }
+    private var password: String { passwordTextField.textField.text ?? "" }
+    
+    // MARK: - Views
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, ageTextField, passwordTextField, registerButton])
+        stackView.distribution = .fill
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var emailTextField: CustomTextField = .init(model: .init(placeholder: "Email",
+                                                                          delegate: self,
+                                                                          status: .normal))
+    
+    private lazy var ageTextField: CustomTextField = .init(model: .init(placeholder: "Age",
+                                                                        delegate: self,
+                                                                        status: .normal))
+    
+    private lazy var passwordTextField: CustomTextField = .init(model: .init(placeholder: "Password",
+                                                                             delegate: self,
+                                                                             status: .normal))
+    
+    
+    private lazy var registerButton: CustomButton = {
+        let button = CustomButton(model: .init(title: "REGISTER"))
+        button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Init
+    
+    public init(viewModel: RegisterViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
-    // MARK: - Properties
-    private let emailTextField: UITextField = {
-        SetUpUI.textField(placeholder: "Email")
-    }()
-    
-    private let invalidEmailLabel: UILabel = {
-        SetUpUI.label(text: "Email is invalid")
-    }()
-    
-    private let ageTextField: UITextField = {
-        SetUpUI.textField(placeholder: "Age")
-    }()
-    
-    private let invalidAgeLabel: UILabel = {
-        SetUpUI.label(text: "Age is invalid")
-    }()
-    
-    private let passwordTextField: UITextField = {
-        SetUpUI.textField(placeholder: "Password")
-    }()
-    
-    private let invalidPasswordLabel: UILabel = {
-        SetUpUI.label(text: "Password is invalid")
-    }()
+   // MARK: - Lifecycle
 
-    private let registerButton: UIButton = {
-        SetUpUI.button(title: "Register")
-    }()
-    
-    private var coreDataManager: CoreDataManagerProtocol!
-//    private var viewModel: RegisterViewModelProtocol!
-    
-    private var email: String { emailTextField.text ?? "" }
-    private var age: String { ageTextField.text ?? "" }
-    private var password: String { passwordTextField.text ?? "" }
-    
-    // MARK: - Lifecycle
+extension RegisterViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        registerButton.addTarget(self,
-                                 action: #selector(registerButtonTapped),
-                                 for: .touchUpInside)
-        
-        emailTextField.delegate = self
-        ageTextField.delegate = self
-        passwordTextField.delegate = self
-        
-        addSubviews()
-        
-        configureViewModel()
+        setupAppearance()
+    }
+}
+
+   //MARK: - Setup
+
+extension RegisterViewController {
+    private func setupAppearance() {
+        navigationController?.title = "Register"
+        self.view.backgroundColor = .systemBackground
+        buildSubviews()
+        buildConstraints()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        //assign frames
-        emailTextField.frame = CGRect(
-            x: 25,
-            y: view.safeAreaInsets.top + 12,
-            width: view.width - 50,
-            height: 48)
-        
-        invalidEmailLabel.frame = CGRect(
-            x: 27,
-            y: emailTextField.bottom + 2,
-            width: view.width - 50,
-            height: 12.0)
-        
-        ageTextField.frame = CGRect(
-            x: 25,
-            y: invalidEmailLabel.bottom + 8,
-            width: view.width - 50,
-            height: 48)
-        
-        invalidAgeLabel.frame = CGRect(
-            x: 27,
-            y: ageTextField.bottom + 2,
-            width: view.width - 50,
-            height: 12.0)
-        
-        passwordTextField.frame = CGRect(
-            x: 25,
-            y: invalidAgeLabel.bottom + 8,
-            width: view.width - 50,
-            height: 48)
-        
-        invalidPasswordLabel.frame = CGRect(
-            x: 27,
-            y: passwordTextField.bottom + 2,
-            width: view.width - 50,
-            height: 12.0)
-        
-        registerButton.frame = CGRect(
-            x: 25,
-            y: invalidPasswordLabel.bottom + 24,
-            width: view.width - 50,
-            height: 48)
+    private func buildSubviews() {
+        view.addSubview(stackView)
     }
     
-    // MARK: - Setup
-    private func addSubviews() {
-        view.addSubview(emailTextField)
-        view.addSubview(invalidEmailLabel)
-        view.addSubview(ageTextField)
-        view.addSubview(invalidAgeLabel)
-        view.addSubview(passwordTextField)
-        view.addSubview(invalidPasswordLabel)
-        view.addSubview(registerButton)
+    private func buildConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16)
+        ])
     }
-    
+}
+
+   //MARK: - Helper
+
+extension RegisterViewController {
     @objc private func registerButtonTapped() {
         // dismiss the keyboard
         emailTextField.resignFirstResponder()
@@ -130,34 +106,28 @@ class RegisterViewController: UIViewController {
                     if CheckValidation.isValidPassword(password) {
                         registerUser()
                     } else {
-                        self.showAlert(with: "Password not Valid")
+                        passwordTextField.setStatus(.error("Password not Valid"))
                     }
                 } else {
-                    self.showAlert(with: "Age not Valid")
+                    emailTextField.setStatus(.error("Age not Valid"))
                 }
             } else {
-                self.showAlert(with: "Mail not Valid")
+                emailTextField.setStatus(.error("Email not Valid"))
             }
         } else {
             self.showAlert(with: "Feel All Fields")
         }
     }
-    
-    private func configureViewModel() {
-        coreDataManager = CoreDataManager()
-//        viewModel = DefaultRegisterViewModel(with: coreDataManager)
-    }
-    
-    // MARK: - Registration Validations
-    private func checkInputs() -> Bool {
-        return emailTextField.hasText || passwordTextField.hasText
-    }
    
     private func registerUser() {
-//        if viewModel.registration(email: email, age: age, password: password) == true {
-//            let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "PhotoGalleryViewController") as? PhotoGalleryViewController
-//            self.navigationController?.pushViewController(vc!, animated: true)
-//        }
+        if viewModel.registration(email: email, age: age, password: password) == true {
+            let vc = PhotoGalleryViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    private func checkInputs() -> Bool {
+        return emailTextField.textField.hasText  || ageTextField.textField.hasText  || passwordTextField.textField.hasText 
     }
     
     private func showAlert(with message: String) {
@@ -172,10 +142,14 @@ class RegisterViewController: UIViewController {
      }
      
      // MARK: - Navigation Method
+    
      private func popToWelcomePage(){
         self.navigationController?.popViewController(animated: true)
      }
 }
+    
+
+   //MARK: - Delegate
 
 extension RegisterViewController: UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
